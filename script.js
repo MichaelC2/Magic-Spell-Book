ab755036bb067529f5e017b246e56ad5a0fadf96
MichaelC2 <44851002+MichaelC2@users.noreply.github.com>
2018-12-05 17:07:10 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+$(".pass").click(function() {
+    $(".balrog").fadeToggle();
+});
+
+$(".Abrakadabra").click(function() {
+    $(".rabbit").slideToggle();
+});
+
+$(".hocus").click(function() {
+    $(".ring").toggle();
+});
+
+
+
+
+
